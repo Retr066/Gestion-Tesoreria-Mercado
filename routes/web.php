d2e1b965088ea75031dd08e83c246ac9a2b84d55
Jherson LP <jherson.lopez88@gmail.com>
2022-09-09 12:57:40 -0500

    Configurando proyecto para deploy

--- routes/web.php
+++ routes/web.php
@@ -28,33 +28,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:sanctum' ])->get('/dashboard', function () {
     return view('users.dashboard');
 })->name('dashboard');
 
 
 
 
-Route::middleware(['auth:sanctum', 'verified'])->get('finanzas/meses/ingresos/{id}',TableIngresos::class ,'render')->where(['id' => '[0-9]+' ])->name('ingresos')->middleware('role:Trabajador|Jefe');
-Route::middleware(['auth:sanctum', 'verified'])->get('finanzas/meses/ingresos/ver/{id}',TableIngresosView::class ,'render')->where(['id' => '[0-9]+' ])->name('ViewIngresos')->middleware('role:Trabajador|Jefe');
+Route::middleware(['auth:sanctum' ])->get('finanzas/meses/ingresos/{id}',TableIngresos::class ,'render')->where(['id' => '[0-9]+' ])->name('ingresos')->middleware('role:Trabajador|Jefe');
+Route::middleware(['auth:sanctum'])->get('finanzas/meses/ingresos/ver/{id}',TableIngresosView::class ,'render')->where(['id' => '[0-9]+' ])->name('ViewIngresos')->middleware('role:Trabajador|Jefe');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('finanzas/meses/egresos/{id}',TableEgresos::class ,'render')->where(['id' => '[0-9]+' ])->name('egresos')->middleware('role:Trabajador|Jefe');
-Route::middleware(['auth:sanctum', 'verified'])->get('finanzas/meses/egresos/ver/{id}',TableEgresosView::class ,'render')->where(['id' => '[0-9]+' ])->name('ViewEgresos')->middleware('role:Trabajador|Jefe');
+Route::middleware(['auth:sanctum'])->get('finanzas/meses/egresos/{id}',TableEgresos::class ,'render')->where(['id' => '[0-9]+' ])->name('egresos')->middleware('role:Trabajador|Jefe');
+Route::middleware(['auth:sanctum'])->get('finanzas/meses/egresos/ver/{id}',TableEgresosView::class ,'render')->where(['id' => '[0-9]+' ])->name('ViewEgresos')->middleware('role:Trabajador|Jefe');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/gestionUsuarios', function () {
+Route::middleware(['auth:sanctum'])->get('/gestionUsuarios', function () {
     return view('admin.gestion');
 })->name('usuarios')->middleware('role:Jefe');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('finanzas/archivados', function () {
+Route::middleware(['auth:sanctum'])->get('finanzas/archivados', function () {
     return view('users.archivados');
 })->name('archivados')->middleware('role:Trabajador|Jefe');
-Route::middleware(['auth:sanctum', 'verified'])->get('balance', function () {
+Route::middleware(['auth:sanctum'])->get('balance', function () {
     return view('users.balanceIngreso');
 })->name('balanceI')->middleware('role:Trabajador|Jefe');
 
 Route::get('balance/BalancePdf/{id}/dowload/{tipo}', [Filtro::class, 'GenerarPdf'])->where(['id' => '[0-9]+' ])->middleware(['middleware' => 'auth:sanctum'])->name('balancePdf')->middleware('role:Trabajador|Jefe');
 
 Route::get('finanzas', [ListReportesController::class, 'index'])->middleware(['middleware' => 'auth:sanctum'])->name('reportes')->middleware('role:Trabajador|Jefe');
-Route::middleware(['auth:sanctum', 'verified'])->get('finanzas/meses/{id}',TableReportes::class ,'render')->where(['id' => '[0-9]+' ])->name('meses')->middleware('role:Trabajador|Jefe');
+Route::middleware(['auth:sanctum'])->get('finanzas/meses/{id}',TableReportes::class ,'render')->where(['id' => '[0-9]+' ])->name('meses')->middleware('role:Trabajador|Jefe');
 Route::get('finanzas/IngresoPdf/{id}/dowload', [TableReportes::class, 'GenerarPdf'])->where(['id' => '[0-9]+' ])->middleware(['middleware' => 'auth:sanctum'])->name('pdf')->middleware('role:Trabajador|Jefe');
 Route::get('finanzas/EgresoPdf/{id}/dowload', [TableReportes::class, 'GenerarPdfEgreso'])->where(['id' => '[0-9]+' ])->middleware(['middleware' => 'auth:sanctum'])->name('pdfEgreso')->middleware('role:Trabajador|Jefe');
